d771fd95bd1afe9f671cd8856b571859a99965a1
honour2ch <you@example.com>
2021-08-21 17:47:41 +0500

    init cytoscape

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,7 +1,31 @@
-class Test {
-    static log(text: string) {
-        document.body.append(text)
-    }
-}
+import cytoscape from "cytoscape"
+import * as utils from './utils'
 
-Test.log('Hello world!')
+cytoscape({
+    container: utils.createCytoscapeContainer(),
+    layout: {
+        name: 'concentric',
+        concentric: function(n: any){ return n.id() === 'j' ? 200 : 0; },
+        levelWidth: function(){ return 100; },
+        minNodeSpacing: 100
+    },
+    elements: {
+        nodes: [
+            { data: { id: 'j', name: 'Jerry' } },
+            { data: { id: 'e', name: 'Elaine' } },
+            { data: { id: 'k', name: 'Kramer' } },
+            { data: { id: 'g', name: 'George' } }
+        ],
+        edges: [
+            { data: { source: 'j', target: 'e' } },
+            { data: { source: 'j', target: 'k' } },
+            { data: { source: 'j', target: 'g' } },
+            { data: { source: 'e', target: 'j' } },
+            { data: { source: 'e', target: 'k' } },
+            { data: { source: 'k', target: 'j' } },
+            { data: { source: 'k', target: 'e' } },
+            { data: { source: 'k', target: 'g' } },
+            { data: { source: 'g', target: 'j' } }
+        ]
+    }
+})
---
+++ client/src/utils.ts
@@ -0,0 +1,14 @@
+export function createCytoscapeContainer() {
+    const container = document.createElement('div')
+
+    container.style.width = '100%'
+    container.style.height = '100%'
+    container.style.position = 'absolute'
+    container.style.left = '0'
+    container.style.top = '0'
+    container.style.zIndex = '999'
+
+    document.body.appendChild(container)
+
+    return container
+}
