96f25f6b842cdc768ce117642ef76fc7e2a26940
honour2ch <you@example.com>
2021-08-21 17:10:53 +0500

    client init

---
+++ client/src/index.ts
@@ -0,0 +1,7 @@
+class Test {
+    static log(text: string) {
+        document.body.append(text)
+    }
+}
+
+Test.log('Hello world!')
---
+++ client/webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        compress: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
